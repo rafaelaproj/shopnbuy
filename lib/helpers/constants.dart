9c19125bfb656cbfc4adbfc4f0bca432ec009fc9
Rafaela Souza <rafaelaproj@gmail.com>
2020-02-27 20:41:39 -0300

    Add files via upload

---
+++ lib/helpers/constants.dart
@@ -0,0 +1,9 @@
+class ViewTitle {
+  static const ProductList = "ShopNBuy";
+  static const Cart = "ShopNBuy's Cart";
+}
+
+class URL {
+  // COMPLETE 1: Place your own Firebase URL here
+  static const ProductList = "https://shopnbuy-77f4a.firebaseio.com/";
+}
